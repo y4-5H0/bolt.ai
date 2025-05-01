cdeebcf3c7e9b3ed49f8e043f2ab62a76bf46fa6
y4-5H0 <nintuto00@gmail.com>
2025-05-02 01:17:49 +0600

    Auto commit - 2025-05-02 01:17:48

--- components/custom/Bottom.jsx
+++ components/custom/Bottom.jsx
@@ -8,13 +8,16 @@ function Bottom() {
             <h2 className='font-bold text-4xl'>{Lookup.HERO_HEADING}</h2>
             <p className='text-gray-400 font-medium'>{Lookup.HERO_DESC}</p>
 
-            <div className='p-5 border rounded-xl max-w-2xl w-full'>
+            <div className='p-5 border rounded-xl max-w-2xl w-full mt-3'>
 
                 <div className='flex gap-2'>
                     <textarea placeholder={Lookup.INPUT_PLACEHOLDER} 
                         className='outline-none bg-transparent w-full h-32 max-h-56 resize-none'
                     />
                     <ArrowRight className='bg-red-500 p-2 h-8 w-8 rounded-md cursor-pointer' />
+                </div>
+                <div>
+                    <Link className='h-5 w-5' />
                 </div>
             </div>
         </div>

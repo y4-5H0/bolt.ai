42995209738187f6d1418630894e8735cfe69c76
y4-5H0 <nintuto00@gmail.com>
2025-05-02 00:59:25 +0600

    Auto commit - 2025-05-02 00:59:25

--- components/custom/Header.jsx
+++ components/custom/Header.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Image from 'next/image'
+import { Button } from '../ui/button'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div>
+        <Image src="/logo.png" alt="Logo" width={40} height={40}  />
+        <div>
+            <Button>Sign In</Button>
+            <Button>Get Started</Button>
+        </div>
+    </div>
   )
 }
 

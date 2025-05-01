26c529da81838304467d3afc2885c7ce5691fd2b
y4-5H0 <nintuto00@gmail.com>
2025-05-02 01:13:43 +0600

    Auto commit - 2025-05-02 01:13:43

--- components/custom/Bottom.jsx
+++ components/custom/Bottom.jsx
@@ -3,20 +3,22 @@ import { ArrowRight } from 'lucide-react'
 import React from 'react'
 
 function Bottom() {
-  return (
-    <div className='flex flex-col items-center justify-center mt-36 xl:mt-52 gap-5'>
-        <h2 className='font-bold text-4xl'>{Lookup.HERO_HEADING}</h2>
-        <p className='text-gray-400 font-medium'>{Lookup.HERO_DESC}</p>
+    return (
+        <div className='flex flex-col items-center justify-center mt-36 xl:mt-52 gap-5'>
+            <h2 className='font-bold text-4xl'>{Lookup.HERO_HEADING}</h2>
+            <p className='text-gray-400 font-medium'>{Lookup.HERO_DESC}</p>
 
-        <div>
+            <div className='p-3'>
 
-        <div className='flex gap-2'>
-            <textarea placeholder={Lookup.INPUT_PLACEHOLDER} />
-            <ArrowRight className='bg-red-500 p-2 h-8 w-8 rounded-md cursor-pointer' />
+                <div className='flex gap-2'>
+                    <textarea placeholder={Lookup.INPUT_PLACEHOLDER} 
+                        className='outline-none bg-transparent w-full h-32 max-h-56 resize-none'
+                    />
+                    <ArrowRight className='bg-red-500 p-2 h-8 w-8 rounded-md cursor-pointer' />
+                </div>
+            </div>
         </div>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Bottom

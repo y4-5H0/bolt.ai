c0e2cac3f72c6d76451e5cea8f7908bbf10a26cd
y4-5H0 <nintuto00@gmail.com>
2025-05-02 01:09:38 +0600

    Auto commit - 2025-05-02 01:09:38

--- components/custom/Bottom.jsx
+++ components/custom/Bottom.jsx
@@ -1,4 +1,5 @@
 import Lookup from '@/data/Lookup'
+import { ArrowRight } from 'lucide-react'
 import React from 'react'
 
 function Bottom() {
@@ -6,6 +7,11 @@ function Bottom() {
     <div className='flex flex-col items-center justify-center mt-36 xl:mt-52 gap-5'>
         <h2 className='font-bold text-4xl'>{Lookup.HERO_HEADING}</h2>
         <p className='text-gray-400 font-medium'>{Lookup.HERO_DESC}</p>
+
+        <div>
+            <textarea placeholder={Lookup.INPUT_PLACEHOLDER} />
+            <ArrowRight className='absolute right-4 top-2 text-gray-400' size={20} />
+        </div>
     </div>
   )
 }

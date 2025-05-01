c68f8fb4bd91560492eba855218eb3a90e7476cb
y4-5H0 <nintuto00@gmail.com>
2025-05-02 01:01:28 +0600

    Auto commit - 2025-05-02 01:01:28

--- components/custom/Header.jsx
+++ components/custom/Header.jsx
@@ -4,10 +4,10 @@ import { Button } from '../ui/button'
 
 function Header() {
   return (
-    <div>
+    <div className='flex justify-between items-center p-4 '>
         <Image src="/logo.png" alt="Logo" width={40} height={40}  />
-        <div>
-            <Button>Sign In</Button>
+        <div className='flex gap-5'>
+            <Button variant='ghost'>Sign In</Button>
             <Button>Get Started</Button>
         </div>
     </div>

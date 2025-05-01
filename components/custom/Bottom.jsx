687e7494b62b89ab68805dc523136aff0cac9923
y4-5H0 <nintuto00@gmail.com>
2025-05-02 01:07:36 +0600

    Auto commit - 2025-05-02 01:07:35

--- components/custom/Bottom.jsx
+++ components/custom/Bottom.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 
 function Bottom() {
   return (
-    <div className='flex flex-col items-center justify-center mt-36 gap-5'>
-        <h2>{Lookup.HERO_HEADING}</h2>
+    <div className='flex flex-col items-center justify-center mt-36 xl:mt-52 gap-5'>
+        <h2 className='font-bold text-4xl'>{Lookup.HERO_HEADING}</h2>
+        <p className='text-gray-400 font-medium'>{Lookup.HERO_DESC}</p>
     </div>
   )
 }

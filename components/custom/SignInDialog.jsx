80394f188c0ff87f6650ee5c2c8c602f46588301
y4-5H0 <nintuto00@gmail.com>
2025-05-02 04:01:45 +0600

    Auto commit - 2025-05-02 04:01:45

---
+++ components/custom/SignInDialog.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+  } from "@/components/ui/dialog"
+  
+
+function SignDialog() {
+  return (
+    <div>SignDialog</div>
+  )
+}
+
+export default SignDialog

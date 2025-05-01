4caba9a0c55d9e37461c6ef5d6fceb0f9d402d34
y4-5H0 <nintuto00@gmail.com>
2025-05-02 00:52:29 +0600

    chore: update dependencies and add PostCSS configuration
    - Updated React and React DOM to version 18.
    - Added new dependencies: @radix-ui/react-slot, class-variance-authority, clsx, lucide-react, next-themes, postcss, tailwind-merge, and tailwindcss.
    - Introduced tw-animate-css as a development dependency.
    - Created a new PostCSS configuration file to integrate Tailwind CSS.

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,6 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import Provider  from "./provider";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Create Next App",
@@ -18,9 +9,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="en" suppressHydrationWarning>
+      <body 
+      >
+
+      <Provider>
         {children}
+        </Provider>
       </body>
     </html>
   );

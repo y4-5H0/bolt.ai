6b83e25f83f10fd0dda3959a8e762c7f3bf2f83c
y4-5H0 <nintuto00@gmail.com>
2025-05-02 01:19:51 +0600

    Auto commit - 2025-05-02 01:19:51

--- components/custom/Bottom.jsx
+++ components/custom/Bottom.jsx
@@ -1,9 +1,10 @@
 import Lookup from '@/data/Lookup'
-import { ArrowRight } from 'lucide-react'
+import { ArrowRight, Link } from 'lucide-react'
 import React from 'react'
 
 function Bottom() {
     return (
+        const 
         <div className='flex flex-col items-center justify-center mt-36 xl:mt-52 gap-5'>
             <h2 className='font-bold text-4xl'>{Lookup.HERO_HEADING}</h2>
             <p className='text-gray-400 font-medium'>{Lookup.HERO_DESC}</p>

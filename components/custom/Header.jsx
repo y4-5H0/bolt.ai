839669753cc02737af1a2cf8e88d6624cdc485e4
y4-5H0 <nintuto00@gmail.com>
2025-05-02 01:03:31 +0600

    Auto commit - 2025-05-02 01:03:30

--- components/custom/Header.jsx
+++ components/custom/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import { Button } from '../ui/button'
+import Colors from '@/data/Colors'
 
 function Header() {
   return (
@@ -8,7 +9,9 @@ function Header() {
         <Image src="/logo.png" alt="Logo" width={40} height={40}  />
         <div className='flex gap-5'>
             <Button variant='ghost'>Sign In</Button>
-            <Button>Get Started</Button>
+            <Button className='text-white' style={{
+                backgroundColor: Colors.RED
+            }}>Get Started</Button>
         </div>
     </div>
   )

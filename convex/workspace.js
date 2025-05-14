62b3ae89fe72fbcb6b317b58279fcfd87cfe169d
y4-5H0 <nintuto00@gmail.com>
2025-05-15 02:12:54 +0600

    2025-05-15 02:12:54

--- convex/workspace.js
+++ convex/workspace.js
@@ -1,4 +1,17 @@
 import { mutation } from "./_generated/server";
+import { v } from "./_generated/values";
 
 export const CreateWorkspace = mutation({
-    args
+    args: {
+        messages: v.any(),
+        user: v.id('users'),
+    },
+    handler: async ({ctx, args}) => {
+        const result = await ctx.db.insert("workspaces", {
+            messages: args.messages,
+            user: args.user,
+        });
+
+    }
+
+    })

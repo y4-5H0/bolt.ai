4ed73c101239658a6654646209c90acc89e9e53a
y4-5H0 <nintuto00@gmail.com>
2025-05-15 03:34:22 +0600

    2025-05-15 03:34:22

--- components/custom/SignInDialog.jsx
+++ components/custom/SignInDialog.jsx
@@ -53,24 +53,6 @@ function SignInDialog({openDialog, closeDialog}) {
   });
 
   return (
-//     <Dialog open={openDialog} onOpenChange={closeDialog}>
-//   <DialogContent>
-//     <DialogHeader>
-//       <DialogTitle></DialogTitle>
-//       <DialogDescription>
-//         <div className='flex flex-col items-center justify-center gap-3'>
-//         <h2 className='font-bold text-2xl text-center text-white'>{Lookup.SIGNIN_HEADING}</h2>
-//         <p className='mt-2 text-center'>{Lookup.SIGNIN_SUBHEADING}</p>
-//         <Button className='bg-red-500 text-white hover:bg-red-400 
-//         mt-3 ' onClick={googleLogin}>Sign In With Google</Button>
-
-//         <p>{Lookup.SIGNIn_AGREEMENT_TEXT}</p>
-
-//         </div>
-//       </DialogDescription>
-//     </DialogHeader>
-//   </DialogContent>
-// </Dialog>
       <Dialog open={openDialog} onOpenChange={closeDialog}>
         <DialogContent>
           <DialogHeader>

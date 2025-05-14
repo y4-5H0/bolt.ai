1b39d27328dfca56bab62db4cd19e0a33b36533e
y4-5H0 <nintuto00@gmail.com>
2025-05-15 02:34:11 +0600

    2025-05-15 02:34:11

--- components/custom/SignInDialog.jsx
+++ components/custom/SignInDialog.jsx
@@ -53,24 +53,55 @@ function SignInDialog({openDialog, closeDialog}) {
   });
 
   return (
-    <Dialog open={openDialog} onOpenChange={closeDialog}>
-  <DialogContent>
-    <DialogHeader>
-      <DialogTitle></DialogTitle>
-      <DialogDescription>
-        <div className='flex flex-col items-center justify-center gap-3'>
-        <h2 className='font-bold text-2xl text-center text-white'>{Lookup.SIGNIN_HEADING}</h2>
-        <p className='mt-2 text-center'>{Lookup.SIGNIN_SUBHEADING}</p>
-        <Button className='bg-red-500 text-white hover:bg-red-400 
-        mt-3 ' onClick={googleLogin}>Sign In With Google</Button>
+//     <Dialog open={openDialog} onOpenChange={closeDialog}>
+//   <DialogContent>
+//     <DialogHeader>
+//       <DialogTitle></DialogTitle>
+//       <DialogDescription>
+//         <div className='flex flex-col items-center justify-center gap-3'>
+//         <h2 className='font-bold text-2xl text-center text-white'>{Lookup.SIGNIN_HEADING}</h2>
+//         <p className='mt-2 text-center'>{Lookup.SIGNIN_SUBHEADING}</p>
+//         <Button className='bg-red-500 text-white hover:bg-red-400 
+//         mt-3 ' onClick={googleLogin}>Sign In With Google</Button>
 
-        <p>{Lookup.SIGNIn_AGREEMENT_TEXT}</p>
+//         <p>{Lookup.SIGNIn_AGREEMENT_TEXT}</p>
 
-        </div>
-      </DialogDescription>
-    </DialogHeader>
-  </DialogContent>
-</Dialog>
+//         </div>
+//       </DialogDescription>
+//     </DialogHeader>
+//   </DialogContent>
+// </Dialog>
+      <Dialog open={openDialog} onOpenChange={closeDialog}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>
+              <h2 className='font-bold text-2xl text-center text-white'>
+                {Lookup.SIGNIN_HEADING}
+              </h2>
+            </DialogTitle>
+
+            <DialogDescription>
+              <p className='mt-2 text-center'>
+                {Lookup.SIGNIN_SUBHEADING}
+              </p>
+            </DialogDescription>
+          </DialogHeader>
+
+          <div className='flex flex-col items-center justify-center gap-3 mt-4'>
+            <Button
+              className='bg-red-500 text-white hover:bg-red-400 mt-3'
+              onClick={googleLogin}
+            >
+              Sign In With Google
+            </Button>
+
+            <p className="text-xs text-center mt-3">
+              {Lookup.SIGNIn_AGREEMENT_TEXT}
+            </p>
+          </div>
+        </DialogContent>
+      </Dialog>
+
 
   )
 }
